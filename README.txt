342c9d19ad33db75b590315822e0a97713c2a64e
Karol Szpek <56266637+karolsz15@users.noreply.github.com>
2020-03-06 03:31:15 +0100

    Add files via upload
    updated styling to make the app responsive (5 breakpoints added), updated background images

---
+++ README.txt
@@ -0,0 +1,35 @@
+The game rules are below:
+
+1. Two players take part in the game, they roll their dices in rounds 
+2. In every single turn, one player rolls two dices as many times as he whishes. Every result is added to his ROUND score
+3. BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+4. The player can choose to 'HOLD' the score, which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+5. The first player WHO reaches set amount of points in his GLOBAL score wins the game
+
+
+Script was written in pure JavaScript, without using any JS framework. I used querySelector.addEventListener methods to manipulate the DOM. 
+Various anonymous functions are run depending on which button is pressed.
+User is allowed to see game rules, start a new game (at any moment!), roll the dices,
+ hold the current score and set the final score, after which the game is over.
+I changed the provided CSS stylesheet significantly, implemented icons and backgrund picture to make it look better. 
+I made many adjustments in the app to be responsive (removed background image, hid some parts of the footer,
+ changed font sizes, moved gaming dices' pictures significantlly, changed paddings and margins etc).
+It works properly on tablets (e.g. iPad's screen resolition) and mobile phones (e.g. iPhone 6/7/8 screen resolutions).
+
+
+![Alt text](/screenshots/desktop1.png?raw=true "Desktop view")
+![Alt text](/screenshots/desktop2.png?raw=true "Desktop view")
+
+![Alt text](/screenshots/tablet-horizontal1.png?raw=true "Tabet horizontal view")
+![Alt text](/screenshots/tablet-horizontal2.png?raw=true "Tabet horizontal view")
+
+![Alt text](/screenshots/tablet-vertical1.png?raw=true "Tablet vertical view")
+![Alt text](/screenshots/tablet-vertical2.png?raw=true "Tablet vertical view")
+
+![Alt text](/screenshots/phone-horizontal1.png?raw=true "Mobile phone horizontal view")
+![Alt text](/screenshots/phone-horizontal2.png?raw=true "Mobile phone horizontal view")
+
+![Alt text](/screenshots/phone-vertical1.png?raw=true "Mobile phone vertical view")
+![Alt text](/screenshots/phone-vertical2.png?raw=true "Mobile phone vertical view")
+
+
---
+++ app.js
@@ -0,0 +1,137 @@
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+var lastDice;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+        
+        // 1. Random number
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+
+        //2. Display the result
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
+        //3. Update the round score IF the rolled number was NOT a 1
+        if (dice1 !== 1 && dice2 !== 1) {
+            //Add score
+            roundScore += dice1 + dice2;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } 
+        else if (dice1 === 1 || dice2 === 1) {
+            //Next player
+            document.getElementById('current-' + activePlayer).textContent = '0!!!';
+            nextPlayer();
+        } 
+        else {
+            //Next player
+            nextPlayer();
+        }
+    }    
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+        
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        
+        // Undefined, 0, null or "" are COERCED to false
+        // Anything else is COERCED to true
+        if(input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
+        
+        // Check if player won the game
+        if (scores[activePlayer] >= winningScore) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
+            document.getElementById('player-current-box-0').style.display = 'none';
+            document.getElementById('player-current-box-1').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            //Next player
+            nextPlayer();
+        }
+    }
+});
+
+
+function nextPlayer() {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-'+ activePlayer).textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
+    document.getElementById('player-current-box-0').style.display = 'block';
+    document.getElementById('player-current-box-1').style.display = 'block';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
